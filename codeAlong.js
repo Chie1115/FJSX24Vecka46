f5af63e0f880930c6d5415783b2288c820167b84
Mandus Lindström <mandus85@hotmail.com>
2024-11-12 10:47:28 +0100

    Done Code along, time for exercise

--- codeAlong.js
+++ codeAlong.js
@@ -6,46 +6,82 @@ console.log("GOOOOD MORNING FJSX24!!!");
 
 // JSON representerar data i form av nyckel/värde-par:
 
+// Grundläggande syntax:
+// {
+//     "name": "Mandus", //string
+//     "age": 39, //number
+//     "isStudent": false, //boolean
+//     "courses": ["html", "css", "vanilla JS"], //array
+//     "adress": {
+//         "street": "Folkungatan",
+//         "city": "Stockholm"
+//     }, //object
+//     "isTeacher": true //boolean
+// }
+
 // -------------------------------------------------------------
 // 2. Arrayer
 
 // Arrayer är listor över värden:
+// {
+//     "fruits": ["äpple", "melon", "orange"],
+//     "numbers": [1, 2, 33]
+// }
 
 // -------------------------------------------------------------
 // 3. Objekt
 
 // Objekt är samlingar av nyckel/värde-par:
 
-// -------------------------------------------------------------
-// 4. Datatyper
-
-// Strängar: Omslutna av dubbla citattecken:
-
-// Nummer: Heltal eller flyttal utan citattecken:
-
-// Booleaner: Sant eller falskt:
-
-// Null: Representerar inget värde:
+// {
+//     "person": {
+//         "name": "Hans",
+//         "age": 2 //Behöver man , här också?
+//     },
+//     "kanManGöraNull": null
+// }
 
 // -------------------------------------------------------------
 // 5. Vanliga Misstag:
 
 // 5A.Använder enkla citattecken istället för dubbla:
 // ❌ Fel:
+// {
+//     'name': "Hans"
+// }
 
 // ✅ Rätt:
+// {
+//     "name": "Hans"
+// }
 
 // -------------------------------------------------------------
 // 5B. Saknade kommatecken mellan nyckel/värde-par:
 // ❌ Fel:
+// {
+//     "name": "Hans"
+//     "age": 3
+// }
 
 // ✅ Rätt:
+// {
+//     "name": "Hans",
+//     "age": 3
+// }
 
 // -------------------------------------------------------------
 // 5C. Sista kommatecknet efter det sista paret:
 // ❌ Fel:
+// {
+//     "name": "Hans",
+//     "age": 3,
+// }
 
-// ✅ Rätt:
+// // ✅ Rätt:
+// {
+//     "name": "Hans",
+//     "age": 3
+// }
 
 // -------------------------------------------------------------
 // -------------------------------------------------------------
@@ -71,12 +107,12 @@ console.log("GOOOOD MORNING FJSX24!!!");
 // Exempel på JavaScript-objekt:
 
 // const person = {
-//     name: 'Olle',
-//     age: 30,
-//     greetings: function() {
-//       console.log('Hej!');
-//     }
-//   };
+//   name: "Olle",
+//   age: 30,
+//   greetings: function () {
+//     console.log("Hej!");
+//   },
+// };
 
 // -------------------------------------------------------------
 // Exempel på motsvarande JSON:
@@ -91,9 +127,28 @@ console.log("GOOOOD MORNING FJSX24!!!");
 
 // JSON.parse(): Konverterar en JSON-sträng till ett JavaScript-objekt:
 
+// const jsonStr = '{"name": "Mandus", "age": 39}';
+
+// console.log("jsonStr", typeof jsonStr);
+
+// const obj = JSON.parse(jsonStr);
+
+// console.log("obj", typeof obj);
+
 // JSON.stringify(): Konverterar ett JavaScript-objekt till en JSON-sträng:
 
+// const obj = {
+//   name: "Hans",
+//   age: 4,
+// };
+// console.log("obj", typeof obj);
+
+// const jsonStr = JSON.stringify(obj);
+// console.log("jsonStr", typeof jsonStr);
+
 // Demo:
+// const jsonStr = '{"name": "Mandus", "age": 39}';
+// let jsonStr22222222222;
 
 // -------------------------------------------------------------
 // Säkerhetshänsyn vid hantering av JSON-data
@@ -103,5 +158,12 @@ console.log("GOOOOD MORNING FJSX24!!!");
 // Se till att data är i förväntat format.
 
 // Använd try-catch vid parsing:
+
+// try {
+//   const obj = JSON.parse(jsonStr);
+//   console.log("obj", obj);
+// } catch (error) {
+//   console.log("error", error);
+// }
 
 // -------------------------------------------------------------
